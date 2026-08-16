6800cb90a341b004a4f932a309fbb2bcdd18e196
Rahul Agrawal <meet4g@gmail.com>
2026-08-17 04:23:36 +0530

    fix: add data set generator

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 import os
+from promptEvaluator import PromptEvaluator
 
 load_dotenv()
 
@@ -28,6 +29,16 @@ client = Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
 
 class ChatRequest(BaseModel):
     messages: list[dict]
+
+class TestCaseRequest(BaseModel):
+    numTestCases: int
+    prompt: str
+    variables: list[str]
+
+class EvaluateRequest(BaseModel):
+    prompt: str
+    testCasesJson: str
+    additionalCriteria: str
 
 @app.get("/")
 def health_check():
@@ -55,7 +66,7 @@ async def chat(request: Request, body: ChatRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/evaluate")
-async def evaluate(request: Request, body: ChatRequest):
+async def evaluate(request: Request, body: EvaluateRequest):
     try:
         response = client.messages.create(
             model="claude-haiku-4-5",
@@ -68,15 +79,18 @@ async def evaluate(request: Request, body: ChatRequest):
         raise HTTPException(status_code=500, detail=str(e))
       
 @app.post("/generate-testcases")
-async def generate_testcases(request: Request, body: ChatRequest):
-	try:
-		response = client.messages.create(
-			model="claude-haiku-4-5",
-			messages=body.messages,
-			max_tokens=500,
-		)
-		return {"testcases": response["completion"]}
+async def generate_testcases(request: Request, body: TestCaseRequest):
+    try:
+        evaluator = PromptEvaluator(max_concurrent_tasks=1, api_key=request.headers.get('X-API-KEY'))
+        dataset = evaluator.generate_dataset(
+            task_description=body.prompt,
+            prompt_inputs_spec=body.variables,
+            output_file="dataset.json",
+            num_cases=body.numTestCases,
+        )
 
-	except Exception as e:
-		raise HTTPException(status_code=500, detail=str(e))
+        return {"testcases": dataset}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
       
